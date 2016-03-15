3c60e2d3315dc6e12783237d2e1a60361cc60ff1
luoye <842891024@qq.com>
2016-03-15 18:08:27 +0800

    整理目录结构｜新增添加元素的模板｜添加文本模块完成

---
+++ Back_Stage/src/js/template/tpl.js
@@ -0,0 +1,35 @@
+'use strict';
+// 必需两个参数 index:当前页元素个数，为了层级
+//             num:所有元素个数，为了id
+module.exports = {
+    txt: function(index, num ,className) {
+        return {
+            type: 'txt',
+            style: 'width:28%;top:0%;left:0%;z-index:' + index + ';color:#1a1a1a;line-height:1.5;padding:1%;font-size:1em;',
+            class: className ? className : '',
+            id: 'n_' + num,
+            attr: {},
+            content: '<p style="display:block;">文案文案文案</p>'
+        };
+    },
+    pic: function(index, num, src, height, width) {
+        return {
+            type: 'pic',
+            style: 'height:' + height + ';width:' + width + ';top:0%;left:0%;z-index:' + index + ';',
+            class: '',
+            id: 'n_' + num,
+            attr: {},
+            content: '<img style="width:100%;height:100%;" src="' + src + '">'
+        };
+    },
+    video: function(index, num, videoid, pic_url) {
+        return {
+            type: 'video',
+            style: 'height:37.5%;width:100%;top:0%;left:0%;z-index:' + index + ';',
+            class: '',
+            id: 'n_' + num,
+            attr: {},
+            content: '<video style="width:100%;height:100%" data-vid="' + videoid + '" poster="' + pic_url + '" controls="controls" preload="none" src="http://video.proc.sina.cn/video_explore/location.php?video_id=' + videoid + '">您的浏览器不支持video标签。</video>'
+        };
+    }
+};
---
+++ Back_Stage/src/js/util/utils.js
@@ -0,0 +1,36 @@
+'use strict';
+var utils = {};
+
+utils.setCookie = function(name, value, minute) {
+    var date = new Date();
+    var minute = minute ? minute : 2;
+    date.setTime(date.getTime() + (minute * 60 * 1000));
+    var expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + "; " + expires;
+};
+
+utils.getCookie = function(name) {
+    var cookies = {};
+    var cookiesArr = document.cookie.replace(/\s+/g, '').split(';');
+    for (var i = 0; i < cookiesArr.length; i++) {
+        var _arr = cookiesArr[i].split('=');
+        cookies[_arr[0]] = _arr[1];
+    }
+    return cookies[name];
+};
+
+utils.delCookie = function(name) {
+    this.setCookie(name, "", -1);
+};
+
+utils.getAllItemsLen = function(mainCode) {
+    if(mainCode){
+        var len = 0;
+        for(var i = 0;i < mainCode.pages.length;i++){
+            len += mainCode.pages[i].items.length;
+        }
+        return len;
+    }
+};
+
+module.exports = utils;
