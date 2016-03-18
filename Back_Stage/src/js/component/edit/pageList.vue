9e4d2bc6eff04ac6dcc96b4dfb62d530edf4dd2f
luoye <842891024@qq.com>
2016-03-18 18:35:59 +0800

    删除本页布局完成

--- Back_Stage/src/js/component/edit/pageList.vue
+++ Back_Stage/src/js/component/edit/pageList.vue
@@ -3,7 +3,10 @@
 		<div class="item pages_list">
 			<div class="head">页面列表</div>
 			<ul>
-				<li v-for="item in pagesData" :class="{'active':currentPage == $index+1}" @click="selectPage($index+1)">第{{$index+1}}页</li>
+				<li v-for="item in pagesData" :class="{'active':currentPage == $index+1}" @click="selectPage($index+1)">
+					<span>第{{$index+1}}页</span>
+					<span class="del_page close" v-show="currentPage == $index+1" @click="delPage($index)">&times;</span>
+				</li>
 			</ul>
 		</div>
 		<div class="item nav_bottom">
@@ -19,9 +22,12 @@
 .pages_con .head {height: 40px;line-height: 40px;text-align: center;color: #fff;background: #0fcebd;font-size: 16px;width: 100%;}
 .pages_con .item.pages_list {flex: 1;}
 .pages_con .item.pages_list ul{position: relative;width: 280px;padding: 0 20px;box-sizing: border-box;overflow-y: auto;flex:1;}
-.pages_con .item.pages_list ul li{display: block;height: 36px;line-height: 36px;margin-top: 10px;color: #666;text-align: center;background: #dcdcdc;cursor: pointer;width: 100%;border-radius: 4px;font-weight: bold;}
+.pages_con .item.pages_list ul li{display: block;height: 36px;line-height: 36px;margin-top: 10px;color: #666;text-align: center;background: #dcdcdc;cursor: pointer;width: 100%;border-radius: 4px;font-weight: bold;position: relative;}
 .pages_con .item.pages_list ul li:last-child{margin-bottom: 10px;}
 .pages_con .item.pages_list ul li.active {background: #0fcebd;color: #fff}
+.pages_con .item.pages_list .del_page{position: absolute;display: block;color: #ff5151;font-size: 27px;right: 7px;top: 4px;z-index: 9;}
+.pages_con .item.pages_list .del_page.close{opacity: 0.8;box-shadow: none;text-shadow: none;}
+.pages_con .item.pages_list .del_page.close:hover{opacity: 1;}
 .pages_con .item.nav_bottom{width: 280px;text-align: center;height: 44px;background: #3f4a58;font-size: 16px;}
 .pages_con .item.nav_bottom a{color: #10c3b3;display: block;cursor: pointer;line-height: 44px;}
 .pages_con .item.nav_bottom a img{display: inline-block;vertical-align: middle;margin-right: 10px;width: 16px;height: 16px;}
@@ -60,7 +66,10 @@ var pageList = Vue.extend({
 	},
 	methods: {
 		selectPage: actions.selectPage,
-		addPage: actions.addPage
+		addPage: actions.addPage,
+		delPage: function(index){
+			
+		}
 	}
 
 })
