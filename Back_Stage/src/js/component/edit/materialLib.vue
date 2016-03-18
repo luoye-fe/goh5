23c418f6b30e34fbf4c3fa9549dd982908f63317
luoye <842891024@qq.com>
2016-03-18 09:37:07 +0800

    素材库添加上传图片按钮及部分逻辑

--- Back_Stage/src/js/component/edit/materialLib.vue
+++ Back_Stage/src/js/component/edit/materialLib.vue
@@ -12,7 +12,10 @@
 					<ul class="group">
 						<li>我的图库</li>
 					</ul>
-					<div class="upload_btn">上传图片</div>
+					<div class="upload_btn">
+						<span>上传图片</span>
+						<input type="file" multiple accept="image/gif, image/jpeg, image/png, image/jpg" @change="uploadImg($event)" style="display: block;width: 100%;height: 100%;position: absolute;opacity: 0;left: 0;top: 0;cursor: pointer;"></input>
+					</div>
 				</div>
 				<div class="lib_main">
 					<div class="lib_main_head">
@@ -49,16 +52,16 @@
 .lib_con .lib_list{width: 200px;display: flex;flex-direction: column;}
 .lib_con .lib_list .group{flex: 1;margin-top: 15px;}
 .lib_con .lib_list .group li{border-bottom: 1px solid #ccd5db;background: #fff;width: 100%;padding: 0 0 0 20px;box-sizing: border-box;color: #76838f;font-size: 12px;height: 44px;line-height: 44px;cursor: pointer;}
-.lib_con .lib_list .upload_btn{width: 100%;height: 44px;line-height: 44px;font-size: 16px;color: #fff;text-align: center;background: #01d7b2;}
+.lib_con .lib_list .upload_btn{width: 100%;height: 44px;line-height: 44px;font-size: 16px;color: #fff;text-align: center;background: #01d7b2;position: relative;}
 .lib_con .lib_main{flex: 1;background: #fff;margin-top: 15px;border-top-left-radius: 6px;padding: 0 15px;}
 .lib_con .lib_main .lib_main_head{width: 100%;height: 44px;border-bottom: 1px solid #ccd5db;display: flex;justify-content: flex-end;}
 .lib_con .lib_main .lib_main_head .lib_main_head_group{font-size: 0;height: 44px;}
 .lib_con .lib_main .lib_main_head .lib_main_head_group li{display: inline-block;vertical-align: middle;font-size: 12px;line-height: 44px;margin: 0 5px;padding: 0 10px;}
-.lib_con .lib_main .lib_main_head .lib_main_head_group li span{cursor: pointer;transition: all ease 0.2s;-webkit-transition: all ease 0.2s;}
+.lib_con .lib_main .lib_main_head .lib_main_head_group li span{cursor: pointer;transition: all ease 0.2s;-webkit-transition: all ease 0.2s;color: #76838f;}
 .lib_con .lib_main .lib_main_head .lib_main_head_group li span:hover{color: #01d7b2;}
 .lib_con .lib_main .lib_main_body{}
 .lib_con .lib_main .lib_main_body .pics_con{font-size: 0;}
-.lib_con .lib_main .lib_main_body .pics_con li{display: inline-table;vertical-align: top;width: 31%;margin: 10px 1% 0;position: relative;overflow: hidden;padding-bottom: 31%;background: #ddd;}
+.lib_con .lib_main .lib_main_body .pics_con li{display: inline-table;vertical-align: top;width: 31%;margin: 10px 1% 0;position: relative;overflow: hidden;padding-bottom: 31%;background: #ddd;cursor: pointer;}
 .lib_con .lib_main .lib_main_body .pics_con li img{display: block;position: absolute;top: 50%;left: 50%;width: 100%;transform: translate(-50%,-50%);-webkit-transform: translate(-50%,-50%);}
 
 </style>
@@ -97,7 +100,23 @@ var MaterialLib = Vue.extend({
 	  	actions: actions
 	},
 	methods:{
-		hideMaterialLib: actions.hideMaterialLib
+		hideMaterialLib: actions.hideMaterialLib,
+		uploadImg: function(ev){
+			var files = {};
+			if(ev.target.files.length > 6){
+				actions.alert(store,{
+					show: true,
+					msg: '一次最多上传6张！超出图片本次不会上传',
+					type: 'warning'
+				})
+				for(var i = 0;i < 6;i++){
+					files[i] = ev.target.files[i];
+				}
+			}else{
+				files = ev.target.files;
+			}
+			console.log(files);
+		}
 	}
 })
 
