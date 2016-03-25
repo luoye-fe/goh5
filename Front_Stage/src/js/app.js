8c6f8171ac3d0fb52d0b6100e7cfc2e27e12d3f9
luoye <842891024@qq.com>
2016-03-25 09:44:38 +0800

    代码整理

--- Front_Stage/src/js/app.js
+++ Front_Stage/src/js/app.js
@@ -1,5 +1,4 @@
 'use strict';
-var dpi = require('./dpi.js');
 
 var load = require('./load.js');
 
--- Front_Stage/src/js/dpi.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-var clientW = document.body.clientWidth;
-var percent = clientW / 320;
-document.getElementById('GoH5ViewPort').setAttribute('content','width=320, initial-scale='+ percent +', maximum-scale='+ percent +', user-scalable=no');
