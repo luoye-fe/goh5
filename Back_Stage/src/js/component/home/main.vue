e4f5a65857cf8b35a70799a1ff2d797cb234bd9b
luoye <842891024@qq.com>
2016-04-08 09:30:21 +0800

    首页样式更新

--- Back_Stage/src/js/component/home/main.vue
+++ Back_Stage/src/js/component/home/main.vue
@@ -1,11 +1,27 @@
 <template>
-	<h1>欢迎来到 Go H5!</h1>
-	<h2>请先登录！</h2>
-	<a href="javascript:void(0)" @click="showLogin = !showLogin">登录</a>
-	<a href="javascript:void(0)" @click="showSignup = !showSignup">注册</a>
-	<m-login :show-login.sync="showLogin" :show-signup.sync="showSignup"></m-login>
-	<m-signup :show-login.sync="showLogin" :show-signup.sync="showSignup"></m-signup>
+	<div class="home_bg"></div>
+	<div class="all_center" style="top: 26%;text-align: center;z-index: 9;left: 22%;">
+		<h1 style="color: #fff;">欢迎来到 Go H5!</h1>
+		<h2 style="color: #fff;">请先登录！</h2>
+		<div class="user_btn">
+			<a href="javascript:void(0)" @click="showLogin = !showLogin">登录</a>
+			<a href="javascript:void(0)" @click="showSignup = !showSignup">注册</a>
+		</div>
+		<m-login :show-login.sync="showLogin" :show-signup.sync="showSignup"></m-login>
+		<m-signup :show-login.sync="showLogin" :show-signup.sync="showSignup"></m-signup>
+	</div>
 </template>
+
+<style>
+.home_bg{position: fixed;top: 0;left: 0;background: url('/back/dist/img/home_bg.jpg') no-repeat;width: 100%;height: 100%;background-size: cover;}
+.user_btn{width: 100%;font-size: 0;text-align: center;}
+.user_btn a{display: inline-block;vertical-align: middle;font-size: 12px;color: #fff;padding: 10px 25px;margin: 0 8px;cursor: pointer;border-radius: 6px;transition: all ease 0.2s;-webkit-transition: all ease 0.2s;}
+.user_btn a:first-child{background: rgba(1,215,178,1);}
+.user_btn a:first-child:hover{background: rgba(1,215,178,0.7);}
+.user_btn a:last-child{background: rgba(8,161,239,1);}
+.user_btn a:last-child:hover{background: rgba(8,161,239,0.7);}
+</style>
+
 
 <script>
 
