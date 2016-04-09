dab54a3b0c5e8f100ac35113fafec59b86cb7bc9
luoye <842891024@qq.com>
2016-04-09 11:09:21 +0800

    键盘操作模块新增

---
+++ Back_Stage/src/js/directive/keyboard.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var Vue = require('Vue');
+var $ = require('jQuery');
+
+var store = require('../store/store.js');
+var actions = require('../store/action/index.js');
+
+var utils = require('utils');
+
+Vue.directive('keyboard',function(){
+	
+})
